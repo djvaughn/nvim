7f6859919531989eaf7af450e5210377f597e58f
Danny Vaughn <daniel.vaughn@tigers.com>
2024-12-04 14:50:54 -0600

    fix autodocs

--- lua/plugins/autodocs.lua
+++ lua/plugins/autodocs.lua
@@ -5,17 +5,23 @@ return {
 		neogen.setup({
 			snippet_engine = "luasnip",
 			enable = true,
-			input_after_comment = true,
 			languages = {
 				python = {
-					annotation_convention = "numpydoc",
+					template = {
+						annotation_convention = "numpydoc",
+					},
 				},
 			},
 		})
 		local opts = { noremap = true, silent = true }
-		vim.keymap.set("n", "<leader>nf", ":lua require('neogen').generate()<CR>", opts)
-		vim.keymap.set("n", "<leader>nc", ":lua require('neogen').generate({ type = 'class' })<CR>", opts)
-		vim.api.nvim_set_keymap("i", "<C-l>", ":lua require('neogen').jump_next<CR>", opts)
-		vim.api.nvim_set_keymap("i", "<C-h>", ":lua require('neogen').jump_prev<CR>", opts)
+		vim.keymap.set("n", "<leader>nf", function()
+			neogen.generate()
+		end, opts)
+		vim.keymap.set("n", "<leader>nc", function()
+			neogen.generate({ type = "class" })
+		end, opts)
+		vim.keymap.set("n", "<leader>nF", function()
+			neogen.generate({ type = "file" })
+		end, opts)
 	end,
 }
--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -14,6 +14,8 @@ return {
 		config = function()
 			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
+			local neogen = require("neogen")
+			local luasnip = require("luasnip")
 
 			cmp.setup({
 				snippet = {
@@ -30,7 +32,39 @@ return {
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							if luasnip.expandable() then
+								luasnip.expand()
+							else
+								cmp.confirm({
+									select = true,
+								})
+							end
+						else
+							fallback()
+						end
+					end),
+
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.locally_jumpable(1) then
+							luasnip.jump(1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
